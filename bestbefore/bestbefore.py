3908e3ef5bcd8ce37b6de5076c7f41e237400fc8
Anton Lazarev <tony@lazarew.me>
2012-10-03 22:24:01 +0400

    Solutions for three puzzles, in pure Python.

---
+++ bestbefore/bestbefore.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# S***y bestbefore puzzle by tony@lazarew.me
+
+import datetime
+import sys
+
+
+class DecodeError(Exception):
+    pass
+
+
+# Data converters/placeholders
+def PH_YEAR(y):
+    'year'
+    if 0 <= y <= 999:
+        return y + 2000
+    elif 2000 <= y <= 2999:
+        return y
+    else:
+        raise DecodeError
+
+
+def PH_MONTH(m):
+    'month'
+    if 1 <= m <= 12:
+        return m
+    else:
+        raise DecodeError
+
+
+def PH_DAY(d):
+    'day'
+    if 1 <= d <= 31:
+        return d
+    else:
+        raise DecodeError
+
+
+# Possible date formats
+FORMATS = (
+    (PH_YEAR, PH_MONTH, PH_DAY),
+    (PH_YEAR, PH_DAY, PH_MONTH),
+    (PH_DAY, PH_MONTH, PH_YEAR),
+    (PH_MONTH, PH_DAY, PH_YEAR),
+
+    # Very unlikely combinations, but still considering them.
+    (PH_DAY, PH_YEAR, PH_MONTH),
+    (PH_MONTH, PH_YEAR, PH_DAY)
+)
+
+
+def calc_date(numbers):
+    """
+    numbers -- list of numbers which are supposed to form a date
+    """
+    valid = []
+    for fmt in FORMATS:
+        try:
+            decoded = dict((
+                (ph.__doc__, ph(numbers[i]))
+                for i, ph in enumerate(fmt)))
+        except DecodeError:
+            continue
+
+        try:
+            date = datetime.date(**decoded)
+        except ValueError:
+            continue
+
+        valid.append(date)
+
+    if valid:
+        date = min(valid)
+        return "%d-%02d-%02d" % (date.year, date.month, date.day)
+    return None
+
+
+def main():
+    input_data = sys.stdin.read().strip()
+    numbers = [int(x) for x in input_data.split("/")]
+
+    assert len(numbers) == 3
+    for n in numbers:
+        assert 0 <= n <= 2999
+
+    date = calc_date(numbers)
+    if date:
+        print(date)
+    else:
+        print("%s is illegal" % input_data)
+
+
+if __name__ == "__main__":
+    main()
---
+++ lottery/lottery.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# S***y lottery puzzle by tony@lazarew.me
+
+import math
+from operator import mul
+import sys
+
+
+def combs(items, slots):
+    if items > slots:
+        return 0
+    return \
+        reduce(mul, xrange(items + 1, slots + 1), 1) \
+        / math.factorial(slots - items + 1)
+
+
+def calc_probability(m, n, t, p):
+    """
+    m -- the total number of people who entered the lottery
+    n -- the total number of winners drawn
+    t -- the number of tickets each winner is allowed to buy
+    p -- the number of people in your group
+    """
+    # w -- required minimum for winners
+    w = int(math.ceil(float(p) / t))
+
+    if w > n:
+        return 0.
+
+    favs = []
+    total = combs(n, m)
+    for i in xrange(0, min(p - w, n - w) + 1):
+        favs.append(combs(w + i, p) * combs(n - w - i, m - p))
+    return math.fsum(favs) / total
+
+
+def main():
+    m, n, t, p = (int(d) for d in sys.stdin.read().split(" "))
+
+    assert 1 <= m <= 1000
+    assert 1 <= n <= m
+    assert 1 <= t <= 100
+    assert 1 <= p <= m
+
+    pretty = "%.10f" % calc_probability(m, n, t, p)
+    if 0 < float(pretty) < 1:
+        pretty = pretty.rstrip("0")
+    print(pretty)
+
+
+if __name__ == "__main__":
+    main()
